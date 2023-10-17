222064299ab401467684b827504c5d5c6378713f
Alessandro F <34175788+tudoale@users.noreply.github.com>
2023-10-17 14:54:28 -0300

    Update WiFimanager_webradio.ino
    Atualizacoes de includes de bibliotecas

--- WiFimanager_webradio/WiFimanager_webradio.ino
+++ WiFimanager_webradio/WiFimanager_webradio.ino
@@ -1,23 +1,14 @@
-#if defined(ESP8266)
-#include <ESP8266WiFi.h>  //ESP8266 Core WiFi Library         
-#else
-#include <WiFi.h>      //ESP32 Core WiFi Library    
-#endif
 
+#include <WiFi.h>      //ESP32 Core WiFi Library   
 #include <HTTPClient.h>
 
 //needed for library
 #include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal ( https://github.com/zhouhan0126/DNSServer---esp32 )
-#if defined(ESP8266)
-#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
-#else
 #include <WebServer.h> //Local WebServer used to serve the configuration portal ( https://github.com/zhouhan0126/WebServer-esp32 )
-#endif
 #include <WiFiManager.h>   // WiFi Configuration Magic ( https://github.com/zhouhan0126/WIFIMANAGER-ESP32 ) >> https://github.com/tzapu/WiFiManager (ORIGINAL)
 
 // Include required libraries
 #include "Arduino.h"
-//#include "WiFi.h"
 #include "Audio.h"
  
 // Define I2S connections
